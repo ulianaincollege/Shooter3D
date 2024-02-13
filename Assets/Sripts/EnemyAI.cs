47e451ad81569547872ce11d67dd92254ce32685
ulianaincollege <ulianaualalova.ranepa@gmail.com>
2024-02-13 22:28:25 +0300

    Add enemy

---
+++ Assets/Sripts/EnemyAI.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> patrolPoints;
+    public PlayerController player;
+    public float viewAngle;
+
+    private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
+
+
+    void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+
+    void Update()
+    {
+        
+        NoticePlayerUpdate();
+        ChaseUpdate();
+        PatrolUpdate();
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+    private void PatrolUpdate()
+    {
+        if(!_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance == 0)
+                PickNewPatrolPoint();
+        }     
+    }
+
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                    _isPlayerNoticed = true;                
+            }
+        }
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+            _navMeshAgent.destination = player.transform.position;
+    }
+}
